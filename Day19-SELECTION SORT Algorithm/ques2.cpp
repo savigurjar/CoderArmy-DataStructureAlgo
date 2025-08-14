54b30859bf46302cff68394a48ffc9079f59df35
savigurjar <savigurjar1201@gmail.com>
2025-08-14 13:58:09 +0530

    day19

---
+++ Day19-SELECTION SORT Algorithm/ques2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[1000];
+    int n;
+    cout << "enter size: ";
+    cin >> n;
+
+    cout << "enter elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] > arr[index])
+            {
+                index = j;
+            }
+        }
+        swap(arr[i], arr[index]);
+    }
+
+    cout << "sorted arry : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
